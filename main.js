0776ed267f261e75ccbe773c3d185b949c695f1b
carsonrp <carsonrp@uw.edu>
2026-01-24 16:52:03 -0800

    moving files over from solo animation assignment, to replace code and images with relevant ones for this project

---
+++ main.js
@@ -0,0 +1,18 @@
+const gameEngine = new GameEngine();
+
+const ASSET_MANAGER = new AssetManager();
+ASSET_MANAGER.queueDownload("./sailormars.png");
+ASSET_MANAGER.queueDownload("./marsattack.png");
+
+ASSET_MANAGER.downloadAll(() => {
+	const canvas = document.getElementById("gameWorld");
+	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+gameEngine.addEntity(new sailormars(gameEngine));
+gameEngine.addEntity(new MarsAttack(gameEngine));
+
+	gameEngine.init(ctx);
+
+	gameEngine.start();
+});
