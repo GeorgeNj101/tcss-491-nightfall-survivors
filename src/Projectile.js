fc25efe36505597720698b1e48f08986901ab0d6
ibrah <ibrahim.elnikety@hotmail.com>
2026-02-12 15:01:18 -0800

    Added Fireball animation and sprite

--- src/Projectile.js
+++ src/Projectile.js
@@ -1,8 +1,10 @@
 import Sprite from "./Sprite.js";
 
 export default class Projectile extends Sprite {
+    rowdir
     constructor(x, y, dx, dy) {
-        super("assets/Bolt.png", { x, y, cols: 1, rows: 1, radius: 30});
+        // super("assets/Bolt.png", { x, y, cols: 1, rows: 1, radius: 30});
+        super("assets/Fireball.png", { x, y, cols: 4, rows: 4, radius: 30});
         
         this.vx = dx; 
         this.vy = dy; 
@@ -11,7 +13,21 @@ export default class Projectile extends Sprite {
         
 
         this.timer = 0;
-        this.maxTime = 200; 
+        this.maxTime = 200;
+
+        if(this.vx===1){
+            if(this.vy===1){
+                this.rowdir=0;
+            }else{
+                this.rowdir=3
+            }
+        }else {
+            if(this.vy===1){
+                this.rowdir=2
+            }else{
+                this.rowdir=1
+            }
+        }
     }
 
     update() {
@@ -26,9 +42,24 @@ export default class Projectile extends Sprite {
     }
 
     draw(ctx, gameFrame, screenX, screenY) {
+
+        if(this.vx<0){
+            if(this.vy>0){
+                this.rowdir=0;
+            }else{
+                this.rowdir=3
+            }
+        }else {
+            if(this.vy<0){
+                this.rowdir=2
+            }else{
+                this.rowdir=1
+            }
+        }
+        let frame = gameFrame % 4
         if (!this.loaded) return;
         // Projectile specific draw: renders smaller than the raw image
-       ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height, screenX, screenY, this.size, this.size);
+       ctx.drawImage(this.image, frame*this.frameWidth, this.rowdir*this.frameHeight, this.frameWidth, this.frameHeight, screenX+this.radius/2-this.frameWidth/2, screenY+this.radius/2-this.frameHeight/2, this.frameWidth, this.frameHeight);
         if (this.showHitbox) {
             ctx.beginPath();
             ctx.strokeStyle = "yellow";
